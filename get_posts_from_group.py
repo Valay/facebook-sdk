7970f73fdc42944ba094ff8430a92d28051a6983
Valay <valayshah26@gmail.com>
2013-08-24 15:19:32 -0400

    It can get all the posts of a member of a group

--- get_posts_from_group.py
+++ get_posts_from_group.py
@@ -6,6 +6,7 @@ import os
 import json
 import time
 import sys
+import codecs
 
 parser = argparse.ArgumentParser(description="Passing the parameters")
 
@@ -15,6 +16,9 @@ parser.add_argument('access_token',type=str,default=None,help='Access Token')
 # Connection type
 parser.add_argument('--conn_type',type=str,default='groups',help='The type of connection')
 
+# Fields required to avoid unnecessary netwrok load
+parser.add_argument('--fields',nargs='*', type=str, help='Fields to filter (to get only those fields in the response)')
+
 # get the group information
 parser.add_argument('--group_id',type=str,help='id of the group')
 parser.add_argument('--group_name',type=str,help='Name of the group')
@@ -23,8 +27,13 @@ parser.add_argument('--group_name',type=str,help='Name of the group')
 parser.add_argument('--member_id',type=str,help='Group member id')
 parser.add_argument('--member_name',type=str,help='Group member name')
 
+# Save path
+parser.add_argument('--savedir', type=str, help='Path to save the posts to...')
+
+graphURL = "https://graph.facebook.com"
+
 def trimURL(url):
-    return url.replace("https://graph.facebook.com","",1)
+    return url.replace(graphURL,"",1)
 
 def get_user(graph):
     return graph.get_object('me')
@@ -63,16 +72,41 @@ def get_group_id(graph, groups, group_name):
     # Return the group id!
     return gid    
     
+def createURL(group_id, data_type, fields):
+    newURL = os.path.join("",group_id,data_type)
+    #newURL += "?fields="
+    #field = ','.join(fields)
+    #newURL += field
+    #print newURL
+    return newURL
+
     
-def get_feed_from_group(graph, group_id):
-    
+def get_feed_from_group(graph, url, member_id):
     if not group_id:
         print "Invalid Group id! Exiting..."
         time.sleep(2)
         sys.exit(0)
     else:
-        feeds = graph.request(os.path.join(group_id,'feed'))
-        print [data['message'] for data in feeds['data'] if data['from']['id'] == '100000153068470']
+        not_done = True
+        while not_done:
+            feeds = graph.request(url)
+            if feeds['data']:
+                filtered = [f for f in feeds['data'] if f['from']['id'] == '100000153068470']
+            else:
+                not_done = False
+            
+            if filtered:
+                for feed in filtered:
+                    #print feed['message']
+                    #feed['message'] = feed['message'].encode('UTF-8')
+                    #print feed['message']
+                    with open(os.path.join(args.savedir,feed['created_time']+".json"),'w') as f:
+                        json.dump(feed,f)
+            
+            # Go to next page
+            nextPage = feeds['paging']['next']
+            url = trimURL(nextPage)
+        
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -91,5 +125,12 @@ if __name__ == '__main__':
     # Search for group
     group_id = get_group_id(graph, groups, group_name=args.group_name)
     
+    # Create URL with fields
+    #url = createURL(group_id,"feed",args.fields)
+    #ags={}
+    #for f in args.fields:
+    #    ags['fields'] = f
+    url = os.path.join(group_id,'feed')
+    #print ags
     #This function will get the feeds from facebook group and save each of them as a single file
-    get_feed_from_group(graph, group_id)
+    get_feed_from_group(graph, url, args.member_id)
